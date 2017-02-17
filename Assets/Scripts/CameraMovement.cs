b491131b7fcb39b6c1da41f22d75487721584cc0
Unknown <srl447@nyu.edu>
2017-02-17 00:01:35 -0500

    Started Working on Movement Code
    It works kinda of but like d is forward :(

---
+++ Assets/Scripts/CameraMovement.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+    float verticalTurn; //vertical camera movement
+    //float horizontalTurn; //horizontal camera movement
+    public float cameraSpeed = 1; //how fast the camera turns
+	// Use this for initialization
+	void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        //float mouseX = Input.GetAxis("Mouse X"); //grabs horizontal mouse inputs
+        float mouseY = Input.GetAxis("Mouse Y");//grabs vertical mouseinputs
+        verticalTurn = -mouseY * Time.deltaTime * cameraSpeed; //assigns value to verticalTurn
+        //horizontalTurn = mouseX * Time.deltaTime * cameraSpeed; //assigns value to verticalTurn
+        transform.Rotate(verticalTurn, 0f, 0f); //rotates camera
+    }
+}
---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    CharacterController characterControl; //makes character controller
+    Vector3 movementDirection; //direction for where the character moves
+    public float speed = 1; //how fast the character moves
+    public float camSpeed = 1; //camera speed for horizontal
+    public float relativeHorizontal;
+    public float relativeVertical;
+    public float angle;
+	void Start ()
+    {
+        characterControl = GetComponent<CharacterController>(); //assigns the character controller
+	}
+	
+	void Update ()
+    {
+        angle = transform.eulerAngles.y * Mathf.PI / 180; //assigns the rotation to angle
+        float horizontal = Input.GetAxis("Horizontal") * speed * Time.deltaTime; //Gets horizontal inputs from a/d and left/right arrows keys
+        float vertical = Input.GetAxis("Vertical") * speed * Time.deltaTime; //Gets vertical inputs from w/s and up/down arrow keys
+        float mouseX = Input.GetAxis("Mouse X"); //Gets horizontal mouse movements
+
+        if (angle > 0)
+        {
+            relativeHorizontal =  horizontal * Mathf.Sin(angle) - vertical * Mathf.Cos(angle);
+            relativeVertical  =  horizontal * Mathf.Cos(angle) + vertical * Mathf.Sin(angle);
+        }
+        //this translates the horizontal and vertical movement to the characters current rotation
+        /*if (angle > 0)
+        {
+            relativeHorizontal = horizontal * Mathf.Cos(angle) - vertical * Mathf.Sin(angle);
+            relativeVertical = horizontal * Mathf.Sin(angle) + vertical * Mathf.Cos(angle);
+        }
+        // accounts for a weird case where a negative angle would occur
+        else
+        {
+            relativeHorizontal = horizontal * Mathf.Cos(angle) + vertical * Mathf.Sin(angle);
+            relativeVertical = - horizontal * Mathf.Sin(angle) + vertical * Mathf.Cos(angle);
+        }*/
+        movementDirection = new Vector3(relativeHorizontal, 0f, relativeVertical); //assigns inputs in a vector
+
+        characterControl.Move(movementDirection);//uses the character controller to move
+        //characterControl.Move(horizontal * Vector3.forward * speed * Time.deltaTime);
+        transform.Rotate(0f, mouseX * Time.deltaTime * camSpeed, 0f);
+        
+    }
+}
