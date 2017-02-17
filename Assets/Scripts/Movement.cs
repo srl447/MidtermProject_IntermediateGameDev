457263f3c9a33a865a22f4a0f72bb5a93ae6e276
Unknown <srl447@nyu.edu>
2017-02-17 00:53:22 -0500

    Fixes the movement scripts
    Readjusted the linear algebra to fix it

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -8,9 +8,9 @@ public class Movement : MonoBehaviour
     Vector3 movementDirection; //direction for where the character moves
     public float speed = 1; //how fast the character moves
     public float camSpeed = 1; //camera speed for horizontal
-    public float relativeHorizontal;
-    public float relativeVertical;
-    public float angle;
+    public float relativeHorizontal; //horizontal movement relative to the player
+    public float relativeVertical;//vertical movement relative to the player
+    public float angle;//angle the player is at in radians
 	void Start ()
     {
         characterControl = GetComponent<CharacterController>(); //assigns the character controller
@@ -18,32 +18,31 @@ public class Movement : MonoBehaviour
 	
 	void Update ()
     {
-        angle = transform.eulerAngles.y * Mathf.PI / 180; //assigns the rotation to angle
+        //sets a bunch of variables to use for math
+        angle = (transform.eulerAngles.y + 90) * Mathf.PI / 180; //assigns the rotation to angle. Converts angle from degrees to radians
+        //the + 90 is to account for the linear algebra creating rotated controls
         float horizontal = Input.GetAxis("Horizontal") * speed * Time.deltaTime; //Gets horizontal inputs from a/d and left/right arrows keys
         float vertical = Input.GetAxis("Vertical") * speed * Time.deltaTime; //Gets vertical inputs from w/s and up/down arrow keys
         float mouseX = Input.GetAxis("Mouse X"); //Gets horizontal mouse movements
 
+        //Adjusts movement direction to be relative to the character direction not the unity world
+        //This is complex linear algebra that I'm not 100% sure on, but I kinda know
         if (angle > 0)
         {
-            relativeHorizontal =  horizontal * Mathf.Sin(angle) - vertical * Mathf.Cos(angle);
-            relativeVertical  =  horizontal * Mathf.Cos(angle) + vertical * Mathf.Sin(angle);
-        }
-        //this translates the horizontal and vertical movement to the characters current rotation
-        /*if (angle > 0)
-        {
-            relativeHorizontal = horizontal * Mathf.Cos(angle) - vertical * Mathf.Sin(angle);
-            relativeVertical = horizontal * Mathf.Sin(angle) + vertical * Mathf.Cos(angle);
+            relativeHorizontal =  horizontal * Mathf.Sin(angle) - vertical * Mathf.Cos(angle); // sets horizontal vector component
+            relativeVertical  =  horizontal * Mathf.Cos(angle) + vertical * Mathf.Sin(angle); //sets vertical vector component
         }
         // accounts for a weird case where a negative angle would occur
         else
         {
-            relativeHorizontal = horizontal * Mathf.Cos(angle) + vertical * Mathf.Sin(angle);
-            relativeVertical = - horizontal * Mathf.Sin(angle) + vertical * Mathf.Cos(angle);
-        }*/
+            relativeHorizontal = horizontal * Mathf.Sin(angle) + vertical * Mathf.Cos(angle);
+            relativeVertical = - horizontal * Mathf.Cos(angle) + vertical * Mathf.Sin(angle);
+        }
         movementDirection = new Vector3(relativeHorizontal, 0f, relativeVertical); //assigns inputs in a vector
 
-        characterControl.Move(movementDirection);//uses the character controller to move
-        //characterControl.Move(horizontal * Vector3.forward * speed * Time.deltaTime);
+        characterControl.Move(movementDirection); //uses the character controller to move
+
+        // rotates the figure to simulate the rotated camera while tracking rotation in the transform
         transform.Rotate(0f, mouseX * Time.deltaTime * camSpeed, 0f);
         
     }
