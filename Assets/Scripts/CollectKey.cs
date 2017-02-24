af159b3e47b4489f9035839ca1a9420b60ab080a
Unknown <srl447@nyu.edu>
2017-02-24 12:59:14 -0500

    Added Models
    Added Desk, chair, and people models, but raycasting broke

--- Assets/Scripts/CollectKey.cs
+++ Assets/Scripts/CollectKey.cs
@@ -17,7 +17,7 @@ public class CollectKey : MonoBehaviour
 	void Update ()
     {
         Ray ray = new Ray(Camera.main.transform.position, Camera.main.transform.forward); // creates a ray
-
+        Debug.Log(Camera.main.transform.position);
         RaycastHit rayHit = new RaycastHit(); //creates variable to store ray data
 
         Debug.DrawRay(ray.origin, ray.direction * 5f, Color.yellow); // draws raycast in the editor
